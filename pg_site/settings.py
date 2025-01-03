2a04298af38547453cfcf0ecf1865604ed8a5c2a
Parker <parker.arneson@gmail.com>
2025-01-03 10:20:34 -0500

    added db url

--- pg_site/settings.py
+++ pg_site/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'pg_site.wsgi.application'
 
 #TODO: Update this to use PostgreSQL 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ("POSTGRES_URL"), 
+        conn_max_age=600
+    )
 }
 
 
