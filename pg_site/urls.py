fb5cbe55547a4d07d5a3e9272dfc0569d2c2444e
Parker <parker.arneson@gmail.com>
2025-02-20 14:23:56 -0500

    removed admin url

--- pg_site/urls.py
+++ pg_site/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("pg_app.urls")),
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
 ]
 
 if not settings.DEBUG:
