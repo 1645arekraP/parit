8ea6f340e0f0d065bf33b6e4c95e60e5b9ef42fe
Parker <parker.arneson@gmail.com>
2025-03-30 17:56:20 -0400

    changed db url

--- pg_site/settings.py
+++ pg_site/settings.py
@@ -91,12 +91,17 @@ WSGI_APPLICATION = 'pg_site.wsgi.application'
 #    },
 #}
 #rit debuging deployment issues with database connection
+#DATABASES = {
+#    'default': dj_database_url.config(
+#        default=f"postgres://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_NAME')}",
+#        conn_max_age=600,
+#        ssl_require=True
+#    )
+#}
+
+# Parker adding more db stuff through DigitalOcean
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"postgres://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_NAME')}",
-        conn_max_age=600,
-        ssl_require=True
-    )
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
 }
 
     
