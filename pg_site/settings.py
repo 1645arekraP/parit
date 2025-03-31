ac0405f8628b0956472a26c16a722b25485afd3a
Parker <parker.arneson@gmail.com>
2025-03-30 23:20:54 -0400

    Trying out new connection

--- pg_site/settings.py
+++ pg_site/settings.py
@@ -90,13 +90,18 @@ WSGI_APPLICATION = 'pg_site.wsgi.application'
 #    },
 #}
 #rit debuging deployment issues with database connection
-DATABASES = {
+"""DATABASES = {
     'default': dj_database_url.config(
         default=f"postgres://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_NAME')}",
         conn_max_age=600,
         ssl_require=True
     )
-}
+}"""
+
+DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("TEST3")),
+    }
+
     
 
 
