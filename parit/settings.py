487377794915500a455f4875cfa71947c88a519e
Parker <parker.arneson@gmail.com>
2025-04-13 21:04:54 -0400

    fixed typo

--- parit/settings.py
+++ parit/settings.py
@@ -95,7 +95,7 @@ CHANNEL_LAYERS = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": f"redis://{os.getenv('REDIS_PASSWORD')}@{os.getenv("REDIS_HOSTT")}:{os.getenv('REDIS_PORT')}",
+        "LOCATION": f"redis://{os.getenv('REDIS_PASSWORD')}@{os.getenv("REDIS_HOST")}:{os.getenv('REDIS_PORT')}",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
             "DECODER_CLASS": "redis.decoder.JSONDecoder",
