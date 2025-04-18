2ca007ae16d3805a76adb81fa4cbf6ddef9973ad
Parker <parker.arneson@gmail.com>
2025-04-18 02:51:44 -0400

    removed debug mode always being true

--- parit/settings.py
+++ parit/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #'PRODUCTION' not in os.environ
+DEBUG = 'PRODUCTION' not in os.environ
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS").split(',')
 
